328c22aec9c767d5268473b9ad1a6eb25e5a7add
Balaji Varadharajan <vbalaji215@gmail.com>
2020-05-04 20:20:19 -0400

    initial commit

---
+++ src/main/java/com/iason/aca/intake/IasonAcaIntakeApplication.java
@@ -0,0 +1,13 @@
+package com.iason.aca.intake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IasonAcaIntakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IasonAcaIntakeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iason/aca/intake/IasonAcaIntakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iason.aca.intake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IasonAcaIntakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
